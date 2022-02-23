6138685fd376904b610ea4defe3fcae2e35b3509
lodv-krsk <dloshenov@ya.ru>
2022-02-23 19:01:08 +0700

    Repository, Manager

---
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -0,0 +1,45 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import java.util.Arrays;
+
+public class TicketManager {
+    private TicketRepository repository;
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public Ticket[] searchBy(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matches(ticket, from, to)) {
+                int length = result.length + 1;
+                Ticket[] tmp = new Ticket[length];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    public boolean matches(Ticket ticket, String from, String to) {
+        if (ticket.getDepartureAirport().contains(from) && ticket.getArrivalAirport().contains(to)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void add(Ticket ticket) {
+        repository.save(ticket);
+    }
+
+    public Ticket[] getAll() {
+        return repository.findAll();
+    }
+}
